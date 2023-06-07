d0fd09cf2a59010e0efe437304dce89f3c197cc5
SANDRO SIMON <sandrosimonore@gmail.com>
2023-06-07 09:59:35 -0500

    remove google maps api credentiasl

--- src/config/index.js
+++ src/config/index.js
@@ -4,4 +4,4 @@ export const server = dev ? 'http://localhost:3000/' : 'https://places.sandrosim
 
 export const mongoDbUri = process.env.MONGODB_URI
 
-export const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+// export const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
